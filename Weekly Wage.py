93cf8ab36ac17f2a2dbd1b736e461aa95024dede
driggz-dev <102887997+driggz-dev@users.noreply.github.com>
2022-05-19 04:54:47 -0400

    Add files via upload

---
+++ Weekly Wage.py
@@ -0,0 +1,11 @@
+# Hours Input
+hrs = input("Enter Hours:")
+
+# Hourly Earnings
+rate = input("Enter Hourly Rate:")
+
+# Weekly Pay
+pay = float(hrs) * float(rate)
+
+# Show me the Money
+print("Pay:", pay)
